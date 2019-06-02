0ded43da9fa10d7820a8acbb7c2613737c1d4ec2
Abtin Bateni <abtinbateni@gmail.com>
2019-06-02 12:52:06 +0430

    add searchRightmostKey for Objects

--- src/main/java/Utility/Utils.java
+++ src/main/java/Utility/Utils.java
@@ -1,5 +1,6 @@
 package Utility;
 
+
 public class Utils {
     public static <T extends Comparable<T>> int searchRightmostKey(final T[] ary, final T key, final int to) {
         int low = 0;
@@ -20,6 +21,33 @@ public class Utils {
                         break;
                     }
                     mid = i;
+                }
+                return mid; // key found
+            }
+        }
+        return -(low + 1); // key not found.
+    }
+
+    public static <T extends Comparable> int searchRightmostKey(final CircularFifoQueue<T> ary, final T key, final int to) {
+        int low = 0;
+        int high = to - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            Comparable<T> midVal = ary.get(mid);
+            int cmp = midVal.compareTo(key);
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                for (int i = mid + 1; i <= high; i++) {
+                    Comparable<T> nxtVal = ary.get(i);
+                    if (midVal.equals(nxtVal)) {
+                        mid = i;
+                    }
+                    else {
+                        break;
+                    }
                 }
                 return mid; // key found
             }
