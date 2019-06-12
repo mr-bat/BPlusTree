6ec0824c6a85497a24a64242ca49f3164c3ff6a8
Abtin Bateni <abtinbateni@gmail.com>
2019-06-12 12:56:33 +0430

    add searchLeftmost

--- src/main/java/Utility/Utils.java
+++ src/main/java/Utility/Utils.java
@@ -3,6 +3,28 @@ package Utility;
 
 public class Utils {
     /**
+     * @return returns the leftmost value greater than or equal to key
+     */
+    public static <T extends Comparable> int searchLeftmostKey(final CircularFifoQueue<T> ary, final T key, final int to) {
+        if (to <= 0)
+            return -1;
+
+        int low = 0;
+        int high = to;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            Comparable<T> midVal = ary.get(mid);
+            int cmp = midVal.compareTo(key);
+            if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low < to && ary.get(low).compareTo(key) == 0 ? low : -(low + 1); // key not found.
+    }
+
+    /**
      * @return returns the rightmost value lower or equal to key
      */
     public static <T extends Comparable> int searchRightmostKey(final CircularFifoQueue<T> ary, final T key, final int to) {
@@ -23,5 +45,4 @@ public class Utils {
         }
         return ary.get(low).compareTo(key) == 0 ? low : -(low + 1); // key not found.
     }
-
 }
--- src/test/java/Utility/UtilsSearchLeftmostTest.java
+++ src/test/java/Utility/UtilsSearchLeftmostTest.java
@@ -4,53 +4,47 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class UtilsTest {
-
-    @BeforeEach
-    void setUp() {
-    }
+class UtilsSearchLeftmostTest {
 
     @Test
     void shouldSearchDistinctKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{1, 2, 3}, 3);
-        int idx = Utils.searchRightmostKey(queue, 1, 3);
+        int idx = Utils.searchLeftmostKey(queue, 1, 3);
         Assertions.assertEquals(0, idx);
     }
 
     @Test
     void shouldFailAtSearchDistinctKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{1, 2, 4}, 3);
-        int idx = Utils.searchRightmostKey(queue, 3, 3);
-        Assertions.assertEquals(-2, idx);
+        int idx = Utils.searchLeftmostKey(queue, 3, 3);
+        Assertions.assertEquals(-3, idx);
     }
 
     @Test
     void shouldSearchMixedKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{1, 2, 2, 5}, 4);
-        int idx = Utils.searchRightmostKey(queue, 2, 4);
-        Assertions.assertEquals(2, idx);
+        int idx = Utils.searchLeftmostKey(queue, 2, 4);
+        Assertions.assertEquals(1, idx);
     }
 
     @Test
     void shouldFailAtSearchMixedKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{1, 2, 2, 5}, 4);
-        int idx = Utils.searchRightmostKey(queue, 3, 4);
-        Assertions.assertEquals(-3, idx);
+        int idx = Utils.searchLeftmostKey(queue, 3, 4);
+        Assertions.assertEquals(-4, idx);
     }
 
     @Test
     void shouldSearchEqualKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{2, 2, 2}, 3);
-        int idx = Utils.searchRightmostKey(queue, 2, 3);
-        Assertions.assertEquals(2, idx);
+        int idx = Utils.searchLeftmostKey(queue, 2, 3);
+        Assertions.assertEquals(0, idx);
     }
 
     @Test
     void shouldFailAtSearchEqualKey() {
         CircularFifoQueue<Integer> queue = new CircularFifoQueue<>(new Integer[]{2, 2, 2}, 3);
-        int idx = Utils.searchRightmostKey(queue, 3, 3);
-        Assertions.assertEquals(-3, idx);
+        int idx = Utils.searchLeftmostKey(queue, 3, 3);
+        Assertions.assertEquals(-4, idx);
     }
 }
