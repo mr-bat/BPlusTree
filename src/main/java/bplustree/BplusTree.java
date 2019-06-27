b0ef65c20ce0f1dbc3e19dfc95c282c8abd16dbc
Abtin Bateni <abtinbateni@gmail.com>
2019-06-27 16:47:23 +0430

    add poll and peek for general use

--- src/main/java/bplustree/BplusTree.java
+++ src/main/java/bplustree/BplusTree.java
@@ -142,4 +142,11 @@ public class BplusTree<Key extends Comparable<Key>, Value> {
 
         return poppedVal;
     }
+    public Value poll() throws BTreeException {
+        return popBack();
+    }
+    @Beta
+    public Value peek() {
+        return peekLastValue();
+    }
 }
